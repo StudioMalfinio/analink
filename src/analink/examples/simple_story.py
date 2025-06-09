63f93184b4574806cf389a2240773600f5377ec9
MalfinioStudio <malfiniostudio@gmail.com>
2025-06-09 15:36:23 +0800

    :art: add an example

--- src/analink/examples/simple_story.py
+++ src/analink/examples/simple_story.py
@@ -1,6 +1,31 @@
 # Simple usage
 from analink.ui.from_textual import run_story_from_text
 
+example_1 = """=== back_in_london ===
+
+We arrived into London at 9.45pm exactly.
+
+*	"There is not a moment to lose!"[] I declared.
+	-> hurry_outside
+
+*	"Monsieur, let us savour this moment!"[] I declared.
+	My master clouted me firmly around the head and dragged me out of the door.
+	-> dragged_outside
+
+*	[We hurried home] -> hurry_outside
+
+
+=== hurry_outside ===
+We hurried home to Savile Row -> as_fast_as_we_could
+
+
+=== dragged_outside ===
+He insisted that we hurried home to Savile Row
+-> as_fast_as_we_could
+
+
+=== as_fast_as_we_could ===
+<> as fast as we could."""
 another_example = """
 -> paragraph_1
 === paragraph_1 ===
@@ -34,7 +59,7 @@ base_example = """
 	"Ah," he replied, not unkindly. "I see you are feeling frustrated. Tomorrow, things will improve."
 """
 run_story_from_text(
-    another_example,
+    example_1,
     typing_speed=0.01,
 )
 
